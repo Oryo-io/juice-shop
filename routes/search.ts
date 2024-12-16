529531f7bc0b668c9d31dd0ee25859eb6ddf6032
tomer-mayara <tomer.mayara@gmail.com>
2024-12-16 15:32:40 -0700

    Update search.ts

--- routes/search.ts
+++ routes/search.ts
@@ -21,6 +21,7 @@ module.exports = function searchProducts () {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     console.log(`## (routes/search.ts, line 23) Searching for Unsanitized input from an HTTP parameter flows into query, where it is used in an SQL query. This may result in an SQL Injection vulnerability.`)
+    console.log(`## (routes/search.ts, line 23) The search criteria is: ${criteria}`)
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
